a88edd18fbf52c7d0726808596020fdcbdd7196f
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-11 00:30:22 +0700

    add: polymorphism

---
+++ Lec43-Polymorphism/src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public void say() {
+        System.out.println("I am an animal");
+    }
+
+    public void walk() {
+        System.out.println("I am walking");
+    }
+}
---
+++ Lec43-Polymorphism/src/Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal{
+    @Override
+    public void say() {
+        System.out.println("I am a dog");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("I am walking on four legs");
+    }
+}
