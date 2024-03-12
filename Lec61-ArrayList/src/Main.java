7b80c3a1323539e6ece10a28d7b9f925f9db575d
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-13 01:51:43 +0700

    add: ArrayList

---
+++ Lec61-ArrayList/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Customer> customers = new ArrayList<Customer>();
+        Customer customer1 = new Customer();
+        customers.add(customer1);
+        System.out.println(customers.get(0));
+
+    }
+}
