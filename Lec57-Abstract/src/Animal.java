7d568be411af81e6e5a557a5a31482dd468d4857
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-13 01:29:23 +0700

    add: abstract file

---
+++ Lec57-Abstract/src/Animal.java
@@ -0,0 +1,10 @@
+public abstract class Animal {
+    // abstract cannot create object
+    private String name;
+    private String dnaCode;
+
+    public void eat() {
+        System.out.println("I am eating");
+    }
+
+}
